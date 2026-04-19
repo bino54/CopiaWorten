4d5e8f5136c0e08e6d0e984e08198c178ccc9a36
bino54 <alvarosilva_54@hotmail.com>
2026-04-19 19:02:30 +0100

    segundo resultado

--- script.js
+++ script.js
@@ -86,3 +86,16 @@ function mostrarProdutos(){
     janelaservico.classList.add('semmostrar');
 }
 
+
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    
+});
